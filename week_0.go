7069f348d7818599328215abfa9b8b65890f0714
udodinho <decagon@Decagons-MacBook-Air.local>
2021-07-27 21:56:09 +0100

    my first assignment

---
+++ week_0.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var name = "Victor"
+	var stack = "Goland"
+	var squad = "sq008"
+	var year = 2021
+
+	fmt.Printf("My name is %v and I am in %v %v %d", name, stack,squad, year)
+}
